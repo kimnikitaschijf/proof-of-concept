5ffe1e59f12b3deb2ac72a3451e99ad0d3199828
kimnikitaschijf <kimnikitaschijfhva@icloud.com>
2025-06-04 20:50:18 +0200

    #2 Server gemaakt, JSON data lokaal gefetched
    De data wordt lokaal opgehaald via de server. Bestand staat in de data map.

--- server.js
+++ server.js
@@ -1,1 +1,53 @@
 console.log('Hier komt je server voor Sprint 12.')
+
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from 'express'
+
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
+
+import { readFile } from 'node:fs/promises'
+
+
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express()
+
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({ extended: true }))
+
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static('public'))
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine('liquid', engine.express());
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
+
+app.get('/', async function (request, response) {
+    const fileContents = await readFile('data/mock-data.json', { encoding: 'utf8' })
+    const fileContentsJSON = JSON.parse(fileContents)
+
+    response.render('index.liquid')
+});
+
+
+app.get('/404', async function (request, response) {
+    response.render('partials/404.liquid')
+})
+
+
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+    // Toon een bericht in de console
+    console.log(`http://localhost:${app.get('port')}/`)
+})
