400b1c359a7fb2e254b854eb7f9c993288cda4f4
kimnikitaschijf <kimnikitaschijfhva@icloud.com>
2025-06-06 14:37:12 +0200

    #2 #14 Server opgeschoond
    Nu overal ; volgens feedback van @julia-stevens

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
-// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
-import express from 'express'
+import express from 'express';
 
-// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
-// Haal de data lokaal op uit de /data map
 // import { readFile } from 'node:fs/promises'
 
-// Maak een nieuwe Express applicatie aan, waarin we de server configureren
-const app = express()
+const app = express();
 
-// Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
-// Bestanden in deze map kunnen dus door de browser gebruikt worden
-app.use(express.static('public'))
-
-// Stel Liquid in als 'view engine'
 const engine = new Liquid();
 app.engine('liquid', engine.express());
 
-// Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
-app.set('views', './views')
-
+app.set('views', './views');
 
 app.get('/', async function (request, response) {
     const apiUrl = 'https://open-jii-api-mock.onrender.com/api/v1/experiments?status=published';
@@ -36,21 +23,15 @@ app.get('/', async function (request, response) {
 
     response.render('index.liquid', {
         experiments,
-        title: 'Gepubliceerde Experimenten'
+        title: 'Published experiments'
     });
 });
 
-
 app.get('/404', async function (request, response) {
-    response.render('partials/404.liquid')
-})
+    response.render('partials/404.liquid');
+});
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
-app.set('port', process.env.PORT || 8000)
-
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.set('port', process.env.PORT || 8000);
 app.listen(app.get('port'), function () {
-    // Toon een bericht in de console
-    console.log(`http://localhost:${app.get('port')}/`)
-})
+    console.log(`http://localhost:${app.get('port')}/`);
+});
