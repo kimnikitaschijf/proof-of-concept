1c31632d009d6b18c9b5fcf469b68f84abb14670
kimnikitaschijf <kimnikitaschijfhva@icloud.com>
2025-06-06 14:44:21 +0200

    #14 #4 Mobile menu werkt nu ook zonder JS
    Feedback van @julia-stevens verwerkt. Nu is het mobile menu altijd toegankelijk, ook als JS niet werkt.

--- public/main.js
+++ public/main.js
@@ -1,4 +1,8 @@
+document.documentElement.classList.remove('no-js');
+document.documentElement.classList.add('js');
+
 function toggleMenu() {
     const nav = document.getElementById("nav-links");
     nav.classList.toggle("open");
 }
+
