fe150e73fcd947e14a60670b7c888d598259d0b9
Kim Nikita Schijf <kimnikitaschijfhva@icloud.com>
2025-06-13 11:41:00 +0200

    #13 Post aangepast
    Nu is er geen redirect naar een lege pagina. Nu gebeurt dat op de pagina zelf

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.engine('liquid', engine.express());
 
 app.set('views', './views');
 
-
 app.get('/', async function (request, response) {
   const apiUrl = 'https://open-jii-api-mock.onrender.com/api/v1/experiments?status=published';
   const fetchResponse = await fetch(apiUrl);
@@ -28,47 +27,35 @@ app.get('/', async function (request, response) {
   });
 });
 
-
-
-
-
 app.get('/:id', async function (request, response) {
-  const id = request.params.id
+  const id = request.params.id;
   const idUrl = `https://open-jii-api-mock.onrender.com/api/v1/experiments?status=published&id=${id}`;
+
   const fetchResponse = await fetch(idUrl);
   const data = await fetchResponse.json();
 
-  response.render('detail.liquid')
+  const experiment = Array.isArray(data) ? data[0] : data;
+
+  response.render('detail.liquid', {
+    experiment
+  });
 });
 
+app.post('/create-experiment', async function (request, response) {
+  const { name, description, data } = request.body;
 
-
-
-
-
-app.post('/create-experiment', function (request, response) {
-  var name = request.body.name;
-  var description = request.body.description;
-  var data = request.body.data;
-
-
-
-  fetch('https://open-jii-api-mock.onrender.com/api/v1/experiments', {
+  await fetch('https://open-jii-api-mock.onrender.com/api/v1/experiments', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ name: name, description: description, data: data })
-  })
-    .then(function (apiResponse) {
-      return apiResponse.json().then(function (result) {
-        if (!apiResponse.ok) {
-          console.error('API error:', result);
-          response.status(apiResponse.status).send('Error creating experiment.');
-        } else {
-          response.send('Experiment successfully created.');
-        }
-      });
-    })
+    body: JSON.stringify({ name, description, data })
+  });
+
+  const fetchResponse = await fetch('https://open-jii-api-mock.onrender.com/api/v1/experiments?status=published');
+  const experiments = await fetchResponse.json();
+
+  response.render('index.liquid', { experiments });
 });
+
 
 app.get('/404', async function (request, response) {
   response.render('partials/404.liquid');
