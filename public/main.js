35aaa1fc29d4a468000d4b940029a5da1f91a3a7
Kim Nikita Schijf <kimnikitaschijfhva@icloud.com>
2025-06-16 11:02:10 +0200

    #22 Comments in JS

--- public/main.js
+++ public/main.js
@@ -1,19 +1,24 @@
+// Classes for whole document
 document.documentElement.classList.remove('no-js');
 document.documentElement.classList.add('js');
 
+// Nav menu on mobile
 function toggleMenu() {
     const nav = document.getElementById("nav-links");
     nav.classList.toggle("open");
 }
 
+// JS for dialog open function 
 const dialog = document.getElementById('experimentDialog');
 const openBtn = document.getElementById('openDialogBtn');
 const closeBtn = document.getElementById('closeDialogBtn');
 
+// Open button (add button)
 openBtn.addEventListener('click', () => {
     dialog.showModal(); 
 });
 
+// Close button
 closeBtn.addEventListener('click', () => {
     dialog.close();
 });
